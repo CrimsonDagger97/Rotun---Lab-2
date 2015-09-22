f8c2ea8eb4a8e310090047201df50de2be9afb53
unknown <4069760@HS222-16.vtsd.com>
2015-09-22 11:53:33 -0400

    my first commit

---
+++ Lab02Java02Cv100.java
@@ -0,0 +1,27 @@
+// Lab02Cv100
+// The 100-point version of Lab 02C is an excercisein precisely copying a
+// provided program, followed by compling the source code into bytecode
+// and executing the bytecode file.
+
+public class Lab02Java02Cv100
+{
+    public static void main (String args[])
+    {
+        System.out.println("Lab 02C, 100 point version");
+        System.out.println();
+        System.out.println("First Java Program");
+        System.out.println();
+        System.out.println("Measures todays knowledge");
+        System.out.println("By yesterdays confusion");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+        System.out.println("# ##### #####   ##### ##### # #   # ##### #####");
+        System.out.println("# #   # #   #   #   # #   # # ##  #   #   #   ");
+        System.out.println("# #   # #   #   ##### #   # # # # #   #   #####");
+        System.out.println("# #   # #   #   #     #   # # #  ##   #       #");
+        System.out.println("# ##### #####   #     ##### # #   #   #   #####");
+        System.out.println();
+    }
+}
+        
---
+++ Lab02Java02Cv80.java
@@ -0,0 +1,21 @@
+// Lab02Cv80
+// The 80-point version of Lab 02C is an excercisein precisely copying a
+// provided program, followed by compling the source code into bytecode
+// and executing the bytecode file.
+
+public class Lab02Java02Cv80
+{
+    public static void main (String args[])
+    {
+        System.out.println("Lab 02C, 80 point version");
+        System.out.println();
+        System.out.println("First Java Program");
+        System.out.println();
+        System.out.println("Measures todays knowledge");
+        System.out.println("By yesterdays confusion");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+    }
+}
+        
